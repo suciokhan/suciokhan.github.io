c6f10196c39b365513b5d4e4c00facc1e5001d33
suciokhan <soldado_z@yahoo.com>
2021-05-13 16:28:47 -0400

    Initial Commit

--- src/script.js
+++ src/script.js
@@ -3,6 +3,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// loading texture (the normal map)
+const textureLoader = new THREE.TextureLoader()
+const normalTexture = textureLoader.load('/textures/NormalMap_sota_logo.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,12 +17,15 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.SphereBufferGeometry(.5, 64, 64)
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.normalMap = normalTexture;
+material.color = new THREE.Color(0x292929)
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
@@ -32,6 +39,70 @@ pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
+// Add a red light
+const pointLight2 = new THREE.PointLight(0xff0000, 2)
+// pointLight2.position.x = 2
+// pointLight2.position.y = 3
+// pointLight2.position.z = 4
+pointLight2.position.set(-1.86,1,-1.65) //altnerative to above 3 lines
+pointLight2.intensity = 10
+
+scene.add(pointLight2)
+
+// make a gui folder for organizing the GUI controls
+const light2 = gui.addFolder('Light 2')
+
+// stuff to let you control light variables via gui!
+light2.add(pointLight2.position, 'x').min(-10).max(10).step(0.01)
+light2.add(pointLight2.position, 'y').min(-10).max(10).step(0.01)
+light2.add(pointLight2.position, 'z').min(-10).max(10).step(0.01)
+light2.add(pointLight2, 'intensity').min(0).max(20).step(0.01)
+
+// setup for color control in GUI is a little more complex...
+const light2Color = {
+    color: 0xff0000
+}
+
+light2.addColor(light2Color, 'color')
+    .onChange(() => {
+        pointLight2.color.set(light2Color.color)
+    })
+
+// const pointLightHelper2 = new THREE.PointLightHelper(pointLight2, 1)
+// scene.add(pointLightHelper2)
+
+// Add another light
+const pointLight3 = new THREE.PointLight(0x90f5, 2)
+// pointLight2.position.x = 2
+// pointLight2.position.y = 3
+// pointLight2.position.z = 4
+pointLight3.position.set(8,-6.6,-6.3) //altnerative to above 3 lines
+pointLight3.intensity = 10
+
+scene.add(pointLight3)
+
+const light3 = gui.addFolder('Light 3')
+
+// stuff to let you control light variables via gui!
+light3.add(pointLight3.position, 'x').min(-10).max(10).step(0.01)
+light3.add(pointLight3.position, 'y').min(-10).max(10).step(0.01)
+light3.add(pointLight3.position, 'z').min(-10).max(10).step(0.01)
+light3.add(pointLight3, 'intensity').min(0).max(20).step(0.01)
+
+// setup for color control in GUI is a little more complex...
+const light3Color = {
+    color: 0xff0000
+}
+
+light3.addColor(light3Color, 'color')
+    .onChange(() => {
+        pointLight3.color.set(light3Color.color)
+    })
+
+// The LightHelper renders where the light is coming from
+// const pointLightHelper3 = new THREE.PointLightHelper(pointLight3, 1)
+// scene.add(pointLightHelper3)
+
 /**
  * Sizes
  */
@@ -40,6 +111,7 @@ const sizes = {
     height: window.innerHeight
 }
 
+// This handles risizing your rendering when the browser window size changes
 window.addEventListener('resize', () =>
 {
     // Update sizes
@@ -73,7 +145,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -82,15 +155,42 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+// make it spion based on mouse input
+
+document.addEventListener('mousemove', onDocumentMouseMove)
+
+let mouseX = 0
+let mouseY = 0
+
+let targetX = 0
+let targetY = 0
+let targetZ = 0
+
+const windowX = window.innerWidth / 2;
+const windowY = window.innerHeight / 2;
+
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowX)
+    mouseY = (event.clientY - windowY)
+}
+
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
+    targetX = mouseX * .001
+    targetY = mouseY * .001
+    targetZ = mouseY * .001
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.y = .5 * elapsedTime // makes it spin all the time
+
+    sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
+    sphere.rotation.x += .05 * (targetY - sphere.rotation.x)
+    sphere.rotation.z += -.05 * (targetZ - sphere.rotation.x)
+
 
     // Update Orbital Controls
     // controls.update()
